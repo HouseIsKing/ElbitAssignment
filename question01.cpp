06a1699af93b043a15611d9cae1af94dec1dbccc
ShiningBlack <amitzxzx@gmail.com>
2022-01-24 12:37:43 +0200

    Question 1 completed

--- question01.cpp
+++ question01.cpp
@@ -12,29 +12,37 @@
 
 #include <chrono>
 #include <iostream>
+#include <numeric>
 #include <vector>
 
 constexpr size_t ELEMENT_COUNT = 1000 * 10000;
 constexpr size_t ITERATIONS = 10;
-
+//Improved complexity from n^2 to n, In essense removed reallocation process on each iteration and instead used the fact the vector size is constant.
+//On my testing I got reduction from ~80K microseconds to only ~38K, that's reduction of at least 50% on the current settings.
 std::vector<uint64_t> fill_vector(size_t elementCount) {
-	//TODO: improve function performance
 	std::vector<uint64_t> vec;
+	vec.reserve(elementCount);
 	for (size_t i = 0; i < elementCount; i++) {
 		vec.push_back(i);
 	}
 	return vec;
 }
 
+
+
 /// This function should return the total elements in all of the vectors
 size_t count_total_elements(const std::vector<std::vector<uint64_t>>& vecOfVec) {
-	return 0ULL; //TODO: replace with your code
+	return std::accumulate(vecOfVec.begin(), vecOfVec.end(), 0, [](int val, std::vector<uint64_t> vec) {return val + vec.size(); });
 }
 
 /// This function should return a single vector that contain all of the elements of the vectors in vecOfVec
 std::vector<uint64_t> merge_vec_of_vec(std::vector<std::vector<uint64_t>>& vecOfVec) {
 	std::vector<uint64_t> mergedVec;
-	//TODO: Your code here
+	mergedVec.reserve(count_total_elements(vecOfVec));
+	for (int i = 0; i < vecOfVec.size(); i++)
+	{
+		copy(vecOfVec[i].begin(), vecOfVec[i].end(), std::back_inserter(mergedVec));
+	}
 	return mergedVec;
 }
 
@@ -42,10 +50,11 @@ int main(int argc, char** argv)
 {
 	//Create vector of vectors
 	std::vector<std::vector<uint64_t>> vecOfVec;
+	vecOfVec.reserve(ITERATIONS);
 	auto start = std::chrono::steady_clock::now();
 	for (size_t i = 0; i < ITERATIONS; i++) {
 		std::vector<uint64_t> vec = fill_vector(ELEMENT_COUNT);
-		//TODO: improve inserting performance
+		//Same as fill_vector, I improved the same way, with the same complexity reduction.
 		vecOfVec.push_back(vec);
 	}
 	auto end = std::chrono::steady_clock::now();
